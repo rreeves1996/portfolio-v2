9f987f2adc571c86b38aeb9a12b4269f344b4cad
Ryan Reeves <reeves0ryan@gmail.com>
2022-06-26 16:23:30 -0700

    Working on adding clicked states to various buttons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,6 +6,9 @@ const pages = [
     portfolio = document.getElementById("portfolio"),
     contact = document.getElementById("contact")];
 
+portfolioButton.addEventListener("click", function() {
+    portfolio.scrollIntoView();
+});
 for (let i = 0; i < pages.length; i++) {
     sideButton[i].addEventListener("click", function() {
         pages[i].scrollIntoView();
